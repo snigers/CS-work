4dd326eef3ccfd1b0a52b6bb2677a23ff62e5cab
snigers <educon@yandex.ru>
2019-04-18 14:51:10 +0400

    test

--- corona_style/js/selectsize.js
+++ corona_style/js/selectsize.js
@@ -2,5 +2,5 @@ function selectChange() {
   var select = $("#selectSizeId").val();
   document.getElementById("selectId").innerHTML = select
   var buy = document.getElementById("selectBuy");
-  buy.setAttribute('data-id', select); 
+  buy.setAttribute('data-id', select);
 }
