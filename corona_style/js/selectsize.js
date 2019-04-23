9d50e64c75fd94ec066bb56e2f5ccef31205e009
snigers <educon@yandex.ru>
2019-04-23 08:58:51 +0400

    Корзина

--- corona_style/js/selectsize.js
+++ corona_style/js/selectsize.js
@@ -3,3 +3,13 @@ function selectChange() {
   var buy = document.getElementById("selectBuy");
   buy.setAttribute('data-id', select);
 }
+
+function show(id){
+      display = document.getElementById(id).style.display;
+      if(display=='none'){
+        document.getElementById(id).style.display='block';
+      }else{
+        document.getElementById(id).style.display='none';
+      }
+    }
+  
